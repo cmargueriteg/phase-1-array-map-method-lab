36800d188058a89527390a206f3a349bd3e7d231
Cindy Germain <cindymarguerite@gmail.com>
2022-03-11 16:41:34 -0700

    commit tree f17a2786608e48b71df3f862f84547df0964b3da

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+
+    let str = tutorial.split(" ")
+    const newArray = str.map(word => word[0].toUpperCase() + word.slice(1))
+
+    return newArray.join(" ")
+
+  })
 }
